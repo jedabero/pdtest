b8fcfaecc2551853544902078b8f36a4bcadacfc
jedabero <jedabero@hotmail.com>
2015-04-17 13:40:32 -0500

    Created migrations

--- polls/models.py
+++ polls/models.py
@@ -2,14 +2,12 @@ from django.db import models
 
 
 class Question(models.Model):
-	"""docstring for Question"""
-	question_text = models.CharField(max_lenght=200)
+	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-	"""docstring for Choice"""
-	question = models.ForeingKey(Question)
-	choice_text = models.CharField(max_lenght=200)
+	question = models.ForeignKey(Question)
+	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
 		
 # Create your models here.
