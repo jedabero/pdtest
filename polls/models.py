f25f5c3b793c53d494fe84812929b553b6cbd279
jedabero <jedabero@hotmail.com>
2015-04-17 13:25:03 -0500

    Created models Question and Choice

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+class Question(models.Model):
+	"""docstring for Question"""
+	question_text = models.CharField(max_lenght=200)
+	pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+	"""docstring for Choice"""
+	question = models.ForeingKey(Question)
+	choice_text = models.CharField(max_lenght=200)
+	votes = models.IntegerField(default=0)
+		
 # Create your models here.
